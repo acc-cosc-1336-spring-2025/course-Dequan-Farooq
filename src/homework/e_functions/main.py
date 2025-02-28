827f08560fbfb24995ffc2e23a30e545edab0a43
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-02-28 02:22:57 +0000

    HW four

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,12 @@
-#
+def convert_seconds_to_time(seconds):
+    hours = seconds // 3600  
+    minutes = (seconds % 3600) // 60  
+    seconds_remaining = seconds % 60  
+
+    time_format = f"{hours:02}:{minutes:02}:{seconds_remaining:02}"
+    return time_format
+
+input_seconds = 3800
+
+time = convert_seconds_to_time(input_seconds)
+print(time)
