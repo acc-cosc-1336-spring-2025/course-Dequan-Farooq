068353b63755948ceb7f2307ab6ebd23cb8f5978
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-04-08 01:05:20 +0000

    writing and reading list of lists

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -77,3 +77,32 @@ def read_employee_records(file_name):
         dept = record [2].rstrip('\n')
         
         print(id, record, dept)
+    file.close()
+
+def write_prog_lang_list_of_lists(file_name):
+    file=open(file_name, 'w')
+
+    prog_langs = [['1980','C++', 'Complex']]
+
+    for lang in prog_langs:
+        file.write(lang[0]+'\t')# year
+        file.write(lang[1]+'\t') # lan
+        file.write(lang[2]+'\t') # diff
+
+    file.close()
+
+def read_prog_lang_list_of_lists(file_name):
+    file= open (file_name, 'r')
+    list_langs= [] 
+    for line in file:
+        record= line.split('\t')
+        year= record[0]
+        lang= record[1]
+        level= record[2]
+
+        lang_list= [year, lang, level]
+        
+        list_langs.append(lang_list)
+    print(list_langs)
+
+    file.close()
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -10,8 +10,15 @@ def main():
     #files.read_from_file_data_w_loop(file_name)
     #files.read_from_file_w_for_(file_name)
 
-    file_name = 'employee.txt'
+    #file_name = 'employee.txt'
 
     #files.write_employee_records(file_name)
-    files.read_employee_records(file_name)
+    #files.read_employee_records(file_name)
+    
+
+    file_name= 'langs.txt'
+    #files.write_prog_lang_list_of_lists(file_name)
+    files.read_prog_lang_list_of_lists(file_name)
+
+
 main()
