a85111cd203a6dcf9a5b91226d88a05903fe3d45
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-04-08 00:27:13 +0000

    making a new file

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,30 @@
-#
+
+def write_to_file(file_name):
+    file= open (file_name, 'w')
+
+    file.write ('John Locke\n')
+    file.write ('David Hume\n')
+    file.write ('Edmund Burke\n')
+
+    file.close
+
+def read_from_file(file_name):
+    file = open(file_name, 'r')
+
+    file_lines= file.read()
+    file.close()
+
+    print(file_lines)
+
+
+def read_from_file_one_line_at_a_time(file_name):
+    file = open(file_name,'r')
+
+    line1= file.readline().rstrip('\n')
+    line2= file.readline().rstrip('\n')
+    line3= file.readline().rstrip('\n')
+
+
+    print(line1)
+    print(line2)
+    print(line3)
