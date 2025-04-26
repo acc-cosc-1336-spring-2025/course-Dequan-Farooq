41cd5956dad02cf72a8ed427b80fc6936f213e0d
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-04-26 16:25:39 +0000

    hm 9

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,13 @@
-#
+import random
+
+class die:
+    __roll_value = None  # private class attribute
+
+    def roll(self):
+        self.__roll_value = random.randint(1, 6)
+
+    def get_rolled_value(self):
+        return self.__roll_value
+
+    def __str__(self):
+        return f"The rolled value is {self.__roll_value}"
